126f88632b3150b2a4238853e0e25a9f24e920de
Albert Kim <albertkim@Alberts-Air.hitronhub.home>
2023-11-26 17:34:23 -0800

    Finally have a consistent menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,85 @@
-import { useState } from 'react'
-import { Layout, Menu, Button } from 'antd'
+import { useState, useEffect, useRef } from 'react'
+import { Layout } from 'antd'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { HomeFooter } from './pages/HomeFooter'
 import { HomePage } from './pages/HomePage'
 import { AboutPage } from './pages/AboutPage'
 import { DemoPage } from './pages/DemoPage'
 import { NewsPage } from './pages/NewsPage'
-import { MenuOutlined } from '@ant-design/icons'
 const { Header, Content, Footer } = Layout
 
 function App() {
 
-  const [expanded, setExpanded] = useState(false)
+  const [menuExpanded, setMenuExpanded] = useState(false)
+  const navRef = useRef<HTMLDivElement>(null);
 
-  const toggleMenu = () => {
-    setExpanded(!expanded);
-    console.log(expanded)
-  };
+  const handleMenuExpanded = () => setMenuExpanded(!menuExpanded)
 
-  const menu = (
-    <Menu mode='horizontal' theme='dark' style={{ display: 'flex', alignItems: 'center' }}>
-      <Menu.Item key='home'>
-        <Link to='/'>GRIDVIEW ANALYTICS</Link>
-      </Menu.Item>
-      <Menu.Item key='about'>
-        <Link to='/about'>ABOUT</Link>
-      </Menu.Item>
-      <Menu.Item key='news'>
-        <Link to='/news'>NEWS</Link>
-      </Menu.Item>
-      <Menu.Item key='demo'>
-        <Link to='/demo'>WAITLIST</Link>
-      </Menu.Item>
-    </Menu>
-  )
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navRef.current && !navRef.current.contains(event.target as Node)) {
+        setMenuExpanded(false)
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [navRef]);
 
   return (
     <BrowserRouter>
       <Layout>
 
-        <Header style={{
+        <div style={{
           position: 'sticky',
           top: 0,
-          zIndex: 1,
+          zIndex: 99,
           width: '100%',
           display: 'flex',
-          alignItems: 'center'
+          alignItems: 'center',
+          backgroundColor: '#001529'
         }}>
 
-          <div className='menu-large'>
-            {menu}
-          </div>
-          <div className={`menu-small ${expanded ? 'visible' : ''}`}>
-            {menu}
-          </div>
+          <nav
+            className='container navbar navbar-expand-md navbar-dark'
+            style={{color: 'white'}}
+            ref={navRef}>
 
-          <Button className='hamburger-button' onClick={toggleMenu} icon={<MenuOutlined />} />
+            <a className='navbar-brand' style={{marginLeft: 15}} href='/'>GRIDVIEW</a>
+
+            <button
+              className='navbar-toggler ml-auto'
+              type='button'
+              data-toggle='collapse' 
+              ata-target='#navbarNav'
+              onClick={handleMenuExpanded}
+              style={{marginRight: 15}}>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+
+            <div className={`${menuExpanded ? '' : 'collapse'} navbar-collapse`}>
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <a className='nav-link' href='/'>Home</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' href='/news'>News</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' href='/about'>About</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' href='/demo'>Waitlist</a>
+                </li>
+              </ul>
+            </div>
+
+          </nav>
           
-        </Header>
+        </div>
+
         <Content style={{backgroundColor: 'white'}}>
 
           <Routes>
